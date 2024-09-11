0d155060b66f9cdb2de597f90cfa338a3346c0b5
akari27 <s2122019@stu.musashino-u.ac.jp>
2024-09-12 01:59:47 +0900

    ログの確認

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -20,10 +20,57 @@ class LocationController extends Controller
         ]);
     }
     
-    public function getNearRamen(Request $request) {
+    // public function getNearRamen(Request $request) {
+    //     $latitude = $request->input('latitude');
+    //     $longitude = $request->input('longitude');
+        
+    //     $ramens = Location::with(['shops' => function($query) use ($latitude, $longitude) {
+    //         $query->selectRaw('shops.*,
+    //         (6371 * acos(cos(radians(?)) * cos(radians(locations.latitude)) * cos(radians(locations.longitude) - radians(?)) + sin(radians(?)) * sin(radians(locations.latitude)))) AS distance',
+    //         [$latitude, $longitude, $latitude])
+    //         ->join('locations', 'shops.location_id', '=', 'locations.id')
+    //         ->orderBy('distance');
+    //     }, 'shops.shop_category'])
+    //         ->selectRaw('locations.*, (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance', [$latitude, $longitude, $latitude])
+    //         ->having('distance', '<', 100)
+    //         ->orderBy('distance')
+    //         ->limit(4)
+    //         ->get();
+        
+    //     $result = $ramens->map(function($ramen) {
+    //         return [
+    //             'latitude' => (float)$ramen->latitude,
+    //             'longitude' => (float)$ramen->longitude,
+    //             'distance' => $ramen->distance,
+    //             'address' => $ramen->address,
+    //             'shops' => $ramen->shops->map(function($shop) {
+    //                 return [
+    //                     'id' => $shop->id,
+    //                     'name' => $shop->name,
+    //                     'open_time' => $shop->open_time,
+    //                     'close_time' => $shop->close_time,
+    //                     'min_price' => $shop->min_price,
+    //                     'max_price' => $shop->max_price,
+    //                     'review_avg' => $shop->review_avg, // 小数点第1位までフォーマット
+    //                     'category_name' => $shop->shop_category ? $shop->shop_category->name : '未分類',
+    //                 ];
+    //             }),
+    //         ];
+    //     });
+        
+    //     return response()->json($result);
+    // }
+    public function getNearRamen(Request $request)
+{
+    try {
+        \Log::info('getNearRamen method started');
+        \Log::info('Request data:', $request->all());
+
         $latitude = $request->input('latitude');
         $longitude = $request->input('longitude');
         
+        \Log::info("Latitude: $latitude, Longitude: $longitude");
+
         $ramens = Location::with(['shops' => function($query) use ($latitude, $longitude) {
             $query->selectRaw('shops.*,
             (6371 * acos(cos(radians(?)) * cos(radians(locations.latitude)) * cos(radians(locations.longitude) - radians(?)) + sin(radians(?)) * sin(radians(locations.latitude)))) AS distance',
@@ -36,14 +83,19 @@ class LocationController extends Controller
             ->orderBy('distance')
             ->limit(4)
             ->get();
-        
+
+        \Log::info('Query executed. Number of results: ' . $ramens->count());
+        \Log::info('Raw query results:', $ramens->toArray());
+
         $result = $ramens->map(function($ramen) {
+            \Log::info('Processing ramen:', $ramen->toArray());
             return [
                 'latitude' => (float)$ramen->latitude,
                 'longitude' => (float)$ramen->longitude,
                 'distance' => $ramen->distance,
                 'address' => $ramen->address,
                 'shops' => $ramen->shops->map(function($shop) {
+                    \Log::info('Processing shop:', $shop->toArray());
                     return [
                         'id' => $shop->id,
                         'name' => $shop->name,
@@ -51,13 +103,20 @@ class LocationController extends Controller
                         'close_time' => $shop->close_time,
                         'min_price' => $shop->min_price,
                         'max_price' => $shop->max_price,
-                        'review_avg' => $shop->review_avg, // 小数点第1位までフォーマット
+                        'review_avg' => $shop->review_avg,
                         'category_name' => $shop->shop_category ? $shop->shop_category->name : '未分類',
                     ];
                 }),
             ];
         });
+
+        \Log::info('Final result:', $result->toArray());
         
         return response()->json($result);
+    } catch (\Exception $e) {
+        \Log::error('Error in getNearRamen: ' . $e->getMessage());
+        \Log::error('Stack trace: ' . $e->getTraceAsString());
+        return response()->json(['error' => 'An error occurred'], 500);
     }
+}
 }
